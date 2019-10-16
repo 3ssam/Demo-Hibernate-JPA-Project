950035948d1ed8ed6be2bd711466addec331e811
Mohamed Essam <ana3ssam2017@gmail.com>
2019-10-16 10:43:22 +0200

    Replace version of mysql to solve error "Client does not support authentication protocol requested by server; consider upgrading MySQL client " and test than get error "unknown entity"

--- src/main/java/com/essam/App.java
+++ src/main/java/com/essam/App.java
@@ -3,6 +3,7 @@ package com.essam;
 import com.essam.models.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 /**
@@ -12,9 +13,9 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Student student = new Student(1,"essam",22,"essam@a.com");
-        Configuration configuration = new Configuration();
+        Configuration configuration = new Configuration().configure().addAnnotatedClass(Student.class);
         SessionFactory factory = configuration.buildSessionFactory();
-        Session session = factory.getCurrentSession();
+        Session session = factory.openSession();
         session.save(student);
     }
 }
