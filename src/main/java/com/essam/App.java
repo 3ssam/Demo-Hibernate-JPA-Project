8781c0f653d2b6414b85eaff464e8ad899083cc5
Mohamed Essam <ana3ssam2017@gmail.com>
2019-10-17 13:06:10 +0200

    Call Stored Procedure by hibernate by to way one without any parameters by using native sql query second way with parameter by use Procedure call

--- src/main/java/com/essam/App.java
+++ src/main/java/com/essam/App.java
@@ -4,9 +4,12 @@ import com.essam.models.Labtop;
 import com.essam.models.Student;
 import org.hibernate.*;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.procedure.ProcedureCall;
+import org.hibernate.procedure.ProcedureOutputs;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
+import javax.persistence.ParameterMode;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -19,33 +22,21 @@ public class App {
         Configuration configuration = new Configuration().configure().addAnnotatedClass(Student.class).addAnnotatedClass(Labtop.class);
         ServiceRegistry registry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
         SessionFactory factory = configuration.buildSessionFactory(registry);
-        Session session = factory.openSession();
-        session.beginTransaction();
 
-        SQLQuery query = session.createSQLQuery("select id ,name,age from student where age > 15");
-        query.setResultTransformer(Criteria.ALIAS_TO_ENTITY_MAP);
-        List<Map> list = query.list();
+        Query q = factory.openSession().createSQLQuery("CALL getAllstudent()").addEntity(Student.class);
+        List<Student> students = q.list();
+        for (Student s : students)
+            System.out.println(s);
 
-        for (Map o : list) {
-            System.out.println(o.get("id") + "    "+o.get("name")+"     "+o.get("age"));
-        }
-
-        query = session.createSQLQuery("select * from student where age > 18");
-        query.addEntity(Student.class);
-        List<Student> list1 = query.list();
-
-        for (Student o : list1) {
-            System.out.println(o);
-        }
-
-        //query = session.createQuery("select id,age,name from Student s where s.id = :param");
-        //query.setParameter("param",9);
-        //Object[] s = (Object[]) query.uniqueResult();
-        //System.out.println(s[0] + " : " + s[1] + " : " + s[2]);
-
-        session.getTransaction().commit();
-        session.close();
-
+        ProcedureCall call = factory.openSession().createStoredProcedureCall("getSomeData");
+        call.registerParameter("P_id",int.class, ParameterMode.IN).bindValue(11);
+        call.registerParameter("P_name",String.class, ParameterMode.OUT);
+        call.registerParameter("P_age",int.class, ParameterMode.OUT);
+        ProcedureOutputs outputs = call.getOutputs();
+        int age = (int)outputs.getOutputParameterValue("P_age");
+        String name = (String)outputs.getOutputParameterValue("P_name");
+        System.out.println(age);
+        System.out.println(name);
 
     }
 }
