6ddaa78d720eb44d2751f82d5135fc20fd83b2b6
Mohamed Essam <ana3ssam2017@gmail.com>
2019-10-17 16:08:37 +0200

    Use JPA by Creat persistence.xml and create configuration to connect with database

--- src/main/java/com/essam/App.java
+++ src/main/java/com/essam/App.java
@@ -1,42 +1,21 @@
 package com.essam;
 
-import com.essam.models.Labtop;
 import com.essam.models.Student;
-import org.hibernate.*;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.procedure.ProcedureCall;
-import org.hibernate.procedure.ProcedureOutputs;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
 
-import javax.persistence.ParameterMode;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().configure().addAnnotatedClass(Student.class).addAnnotatedClass(Labtop.class);
-        ServiceRegistry registry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
-        SessionFactory factory = configuration.buildSessionFactory(registry);
-
-        Query q = factory.openSession().createSQLQuery("CALL getAllstudent()").addEntity(Student.class);
-        List<Student> students = q.list();
-        for (Student s : students)
-            System.out.println(s);
-
-        ProcedureCall call = factory.openSession().createStoredProcedureCall("getSomeData");
-        call.registerParameter("P_id",int.class, ParameterMode.IN).bindValue(11);
-        call.registerParameter("P_name",String.class, ParameterMode.OUT);
-        call.registerParameter("P_age",int.class, ParameterMode.OUT);
-        ProcedureOutputs outputs = call.getOutputs();
-        int age = (int)outputs.getOutputParameterValue("P_age");
-        String name = (String)outputs.getOutputParameterValue("P_name");
-        System.out.println(age);
-        System.out.println(name);
-
+        //Student student = new Student(111,"essam",22,"essam@a.com");
+        Student student = null;
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("TestPersistence");
+        EntityManager manager = factory.createEntityManager();
+        student =  manager.find(Student.class,9);
+        System.out.println(student);
     }
 }
