a96edad22ad53078eb8c4fbd223090dd958e1846
Mohamed Essam <ana3ssam2017@gmail.com>
2019-10-17 12:03:51 +0200

    Using Native SQL Query in hibernate to fetch Special data depend on condition by 2 ways for achieve that

--- src/main/java/com/essam/App.java
+++ src/main/java/com/essam/App.java
@@ -2,14 +2,13 @@ package com.essam;
 
 import com.essam.models.Labtop;
 import com.essam.models.Student;
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import org.hibernate.*;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 /**
@@ -23,17 +22,26 @@ public class App {
         Session session = factory.openSession();
         session.beginTransaction();
 
-        Query query = session.createQuery("from Student where age > 15");
-        List<Student> list = query.list();
+        SQLQuery query = session.createSQLQuery("select id ,name,age from student where age > 15");
+        query.setResultTransformer(Criteria.ALIAS_TO_ENTITY_MAP);
+        List<Map> list = query.list();
 
-        for (Student student : list) {
-            //System.out.println(student);
+        for (Map o : list) {
+            System.out.println(o.get("id") + "    "+o.get("name")+"     "+o.get("age"));
         }
 
-        query = session.createQuery("select id,age,name from Student s where s.id = :param");
-        query.setParameter("param",9);
-        Object[] s = (Object[]) query.uniqueResult();
-        System.out.println(s[0] + " : " + s[1] + " : " + s[2]);
+        query = session.createSQLQuery("select * from student where age > 18");
+        query.addEntity(Student.class);
+        List<Student> list1 = query.list();
+
+        for (Student o : list1) {
+            System.out.println(o);
+        }
+
+        //query = session.createQuery("select id,age,name from Student s where s.id = :param");
+        //query.setParameter("param",9);
+        //Object[] s = (Object[]) query.uniqueResult();
+        //System.out.println(s[0] + " : " + s[1] + " : " + s[2]);
 
         session.getTransaction().commit();
         session.close();
